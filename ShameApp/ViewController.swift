1b94aabefe7950fb188c389f14645dbcb5166773
sou1t <asus527@ya.ru>
2016-10-31 15:49:36 +0300

    Initial Commit

---
+++ ShameApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShameApp
+//
+//  Created by Виталий Волков on 31.10.16.
+//  Copyright © 2016 Виталий Волков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
